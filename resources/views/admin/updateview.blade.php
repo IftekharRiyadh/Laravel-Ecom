1789e492af503e08c7ca95cb76cbedf37923155f
iftekharriyadh <iftekharriyadh@gmail.com>
2021-11-29 13:06:33 +0600

    laravel Ecom

--- resources/views/admin/updateview.blade.php
+++ resources/views/admin/updateview.blade.php
@@ -2,8 +2,8 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+      <base href="/public">
     @include('admin.css')
-
     <style type="text/css">
     .title{
         "color:white; 
@@ -33,7 +33,6 @@
         <div class="container-fluid page-body-wrapper">
             <div class="container" align="center">
       <h1 class="title">Add Product</h1>
-      
       @if(session()->has('message'))
       <div class="alert alert-success">
           <button type="button" class="close" data-dismiss="alert">x</button>
@@ -41,34 +40,40 @@
       </div>
       @endif
 
-      <form action="{{url('uploadproduct')}}" method="post" enctype="multipart/form-data">
+      <form action="{{url('updateproduct',$data->id)}}" method="post" enctype="multipart/form-data">
       @csrf
 
       <div style="padding:15px;">
           <label>Product title</label>
-          <input style="color:black;" type="text" name="title" placeholder="Give a product title"
+          <input style="color:black;" type="text" name="title" value="{{$data->title}}"
           required="">
       </div>
 
       <div style="padding:15px;">
           <label>Price</label>
-          <input style="color:black;" type="number" name="price" placeholder="Give a product price"
+          <input style="color:black;" type="number" name="price" value="{{$data->price}}"
           required="">
       </div>
 
       <div style="padding:15px;">
           <label>Description</label>
-          <input style="color:black;" type="text" name="des" placeholder="Give a description"
+          <input style="color:black;" type="text" name="des" value="{{$data->description}}"
           required="">
       </div>
 
       <div style="padding:15px;">
           <label>Quantity</label>
-          <input style="color:black;" type="text" name="quantity" placeholder="Product Quantity"
+          <input style="color:black;" type="text" name="quantity" value="{{$data->quantity}}"
           required="">
       </div>
 
       <div style="padding:15px;">
+          <label>Old Image</label>
+         <img height="100" weight="100" src="{{ asset('app/'.$data->image)}}">
+      </div>
+
+      <div style="padding:15px;">
+      <label>Change The Image</label>
          
           <input type="file" name="product_file">
       </div>
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,15 @@ route::get('/redirect',[HomeController::class,'redirect']);
 route::get('/',[HomeController::class,'index']);
 route::get('/product',[AdminController::class,'product']);
 route::post('/uploadproduct',[AdminController::class,'uploadproduct']);
+
+route::get('/showproduct',[AdminController::class,'showproduct']);
+route::get('/deleteproduct/{id}',[AdminController::class,'deleteproduct']);
+route::get('/updateview/{id}',[AdminController::class,'updateview']);
+route::post('/updateproduct/{id}',[AdminController::class,'updateproduct']);
+route::get('/search/',[HomeController::class,'search']);
+route::post('/addcart/{id}',[HomeController::class,'addcart']);
+route::get('/showcart/',[HomeController::class,'showcart']);
+route::get('/delete/{id}',[HomeController::class,'deletecart']);
+route::post('/order/',[HomeController::class,'confirmorder']);
+route::get('/showorder/',[AdminController::class,'showorder']);
+route::get('/updatestatus/{id}',[AdminController::class,'updatestatus']);
